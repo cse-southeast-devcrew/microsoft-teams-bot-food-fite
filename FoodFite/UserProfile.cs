d7361a11b740a9654a33072651a4658bd30ee5ea
Ashley Ellis <apellis96@gmail.com>
2020-10-30 16:19:01 +0000

    Updated EmptyBot to return user name

---
+++ FoodFite/UserProfile.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Schema;
+
+namespace EmptyBot
+{
+    /// <summary>
+    /// This is our application state. Just a regular serializable .NET class.
+    /// </summary>
+    public class UserProfile
+    {
+
+        public string Name { get; set; }
+
+    }
+}
