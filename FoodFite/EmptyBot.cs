5014b9ec49a53cb8cc654f4177cfadd86212c0f7
AshEllis96 <73497992+AshEllis96@users.noreply.github.com>
2020-11-02 09:16:11 -0500

    Updated EmptyBot to return user name (#16)
    Co-authored-by: Ian Philpot <ian.philpot@microsoft.com>

--- FoodFite/EmptyBot.cs
+++ FoodFite/EmptyBot.cs
@@ -7,21 +7,54 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Schema;
 
 namespace EmptyBot
 {
-    public class EmptyBot : ActivityHandler
+    public class EmptyBot<T> : ActivityHandler where T: Dialog
     {
-        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        protected readonly Dialog Dialog;
+        protected readonly BotState ConversationState;
+        protected readonly BotState UserState;
+
+        public EmptyBot(ConversationState conversationState, UserState userState, T dialog)
         {
-            foreach (var member in membersAdded)
-            {
-                if (member.Id != turnContext.Activity.Recipient.Id)
-                {
-                    await turnContext.SendActivityAsync(MessageFactory.Text($"Hello world!"), cancellationToken);
-                }
-            }
+            ConversationState = conversationState;
+            UserState = userState;
+            Dialog = dialog;
         }
+
+        public override async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default)
+        {
+            await base.OnTurnAsync(turnContext, cancellationToken);
+
+            // Save any state changes that might have occurred during the turn.
+            await ConversationState.SaveChangesAsync(turnContext, false, cancellationToken);
+            await UserState.SaveChangesAsync(turnContext, false, cancellationToken);
+        }
+
+        // protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        // {
+           
+        //     foreach (var member in membersAdded)
+        //     {
+        //         if (member.Id != turnContext.Activity.Recipient.Id)
+        //         {
+        //             await Dialog.RunAsync(turnContext, ConversationState.CreateProperty<DialogState>(nameof(DialogState)), cancellationToken);
+
+        //             //await turnContext.SendActivityAsync(MessageFactory.Text($"Hello world!"), cancellationToken);
+        //         }
+        //     }
+        // }
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+
+            // Run the Dialog with the new message Activity.
+            await Dialog.RunAsync(turnContext, ConversationState.CreateProperty<DialogState>(nameof(DialogState)), cancellationToken);
+        }
+
+        
     }
 }
--- FoodFite/Startup.cs
+++ FoodFite/Startup.cs
@@ -29,9 +29,22 @@ namespace EmptyBot
 
             // Create the Bot Framework Adapter with error handling enabled.
             services.AddSingleton<IBotFrameworkHttpAdapter, BotFrameworkHttpAdapter>();
+            
+            // Create the storage we'll be using for User and Conversation state. (Memory is great for testing purposes.)
+            services.AddSingleton<IStorage, MemoryStorage>();
+
+             // Create the User state. (Used in this bot's Dialog implementation.) ; DialogBot.cs
+            services.AddSingleton<UserState>(); 
+
+            // Create the Conversation state. (Used by the Dialog system itself.) ; DialogBot.cs
+            services.AddSingleton<ConversationState>();
+
+            // The Dialog that will be run by the bot.
+            services.AddSingleton<UserProfileDialog>();
 
             // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
-            services.AddTransient<IBot, EmptyBot>();
+            services.AddTransient<IBot, EmptyBot<UserProfileDialog>>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
