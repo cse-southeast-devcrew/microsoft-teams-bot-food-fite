e934c90c3e711f2f3697bd1bd69584f286cf517f
James Truitt <54150331+jamesptruitt@users.noreply.github.com>
2020-11-03 10:30:12 -0500

    Jtruit/morefight (#31)
    * Fight
    
    * More Fight  Stuff
    
    Co-authored-by: James Truitt <>

--- FoodFite/Models/Cafeteria.cs
+++ FoodFite/Models/Cafeteria.cs
@@ -1,9 +1,19 @@
 namespace FoodFite.Models
 {
     using System;
+    using System.Collections.Generic;
 
-    class Cafeteria
+    public class Cafeteria
     {
+        
+        private List<String> _users = new List<String>() {"Bubba", "Kyle", "Kurt"};
+            
+        public List<string> Users
+        {
+            get { return _users; }
+            set { _users = value; }
+        }
+        
         public string Name { get; set; }
         public int MaxNumberOfThrowsInAFite { get; set; }
         public decimal AmountOfDailyLunchMoney { get; set; }
