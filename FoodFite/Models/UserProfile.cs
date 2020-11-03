2c2a27c956700610f47ac70df9b3e46f58f3dd4a
David Lambright <ladavid@microsoft.com>
2020-11-03 17:03:36 +0000

    Add functions to users

--- FoodFite/Models/UserProfile.cs
+++ FoodFite/Models/UserProfile.cs
@@ -2,6 +2,8 @@
 // Licensed under the MIT License.
 
 using Microsoft.Bot.Schema;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FoodFite.Models
 {
@@ -10,12 +12,53 @@ namespace FoodFite.Models
     /// </summary>
     public class UserProfile
     {
-
         public string Name { get; set; }
-        
-        public string Opponent { get; set; }
+        public List<Item> Inventory { get; set;}
+        public double Health { get; set;}
+        public Protection Clothes { get; set; }
 
-        public string Weapon { get; set; }
+
+        public List<Item> ListFood(){
+            return Inventory.Where(x => x.Throwable == true).ToList<Item>();
+        }
+
+        public List<Item> ListProtection(){
+            List<Item> list = Inventory.Where(x => x.Throwable == false).ToList<Item>();
+            list.Add(Clothes);
+            return list;
+        }
+
+        public void ChangeClothes(Protection newClothes){
+            Protection oldClothes = Clothes;
+            Inventory.Remove(newClothes);
+            Clothes = newClothes;
+            if(oldClothes != null){
+                Inventory.Add(oldClothes);
+            }
+        }
+
+        public double ThrowFood(Food food, UserProfile target){
+            double damage = food.Attack(0);
+
+            if(!food.hasAmmo()){
+                Inventory.Remove(food);
+            }
+            
+            return target.GetHit(damage);
+        }
+
+        public double GetHit(double damage){
+            
+            double damageDone = (Clothes == null) ? damage : Clothes.TakeDamage(damage, 0);
+            
+            if(Clothes.isBroken()){
+                Clothes = null;
+            }
+
+            Health -= damageDone;
+
+            return damageDone;
+        }
 
     }
 }
